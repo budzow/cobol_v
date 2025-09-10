025dfeb66d088480deb8d91953a3506313002b31
Michal Budzowski <Michal.Budzowski@sonarsource.com>
2025-09-10 10:27:34 +0200

    comment update

--- COMPX.cbl
+++ COMPX.cbl
@@ -6,7 +6,7 @@
       
        01  T00 PIC X COMP-X VALUE 062. *> PIC COMP-X is a numeric type, not alphanumeric one 
 	                                   *> so cobol:COBOL.DataItemInitialValueCheck should not 
-									   *> raise an issue when such field is initialized with a numeric value
+									   *> raise an issue when such field is initialized with a numeric value.
        PROCEDURE DIVISION.
        MAIN-LOGIC.                
            DISPLAY T00.                      
